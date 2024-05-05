a7a7dfe3e5716005d13e556698069af63ac52c72
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2024-05-05 18:40:06 +0200

    app CBV del FBV

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,34 +1,41 @@
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from .forms import BirthdayForm
-from .utils import calculate_birthday_countdown
 from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayListView(ListView):
+    breakpoint()
     model = Birthday
     ordering = 'id'
-    paginate_by = 2
+    paginate_by = 4
 
 
-class BirthdayMixin:
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayFormMixin:
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
+class BirthdayDetailView(DetailView):
+    model = Birthday
 
-
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
-
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
